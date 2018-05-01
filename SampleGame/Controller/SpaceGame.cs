97948504b5e0a07becdaabbec6144ce6f62f9415
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-05-01 08:19:45 -0600

    added the enimeies to the game

--- SampleGame/Controller/SpaceGame.cs
+++ SampleGame/Controller/SpaceGame.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+using System.Collections.Generic;
+
 using SampleGame.Model;
 using SampleGame.View;
 
@@ -37,6 +39,17 @@ namespace SampleGame.Controller
 		private ParallaxingBackground bgLayer1;
 		private ParallaxingBackground bgLayer2;
 
+		// Enemies
+		private Texture2D enemyTexture;
+		private List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+
+		// A random number generator
+		private Random random;
+
 
 		public SpaceGame()
 		{
@@ -57,6 +70,18 @@ namespace SampleGame.Controller
 
 			bgLayer1 = new ParallaxingBackground();
 			bgLayer2 = new ParallaxingBackground();
+
+			// Initialize the enemies list
+			enemies = new List<Enemy>();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
 
 			base.Initialize();
 		}
@@ -83,6 +108,8 @@ namespace SampleGame.Controller
 			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
 
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
+
+			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
 		}
 
 		/// <summary>
@@ -114,6 +141,9 @@ namespace SampleGame.Controller
 			bgLayer1.Update();
 			bgLayer2.Update();
 
+			// Update the enemies
+			UpdateEnemies(gameTime);
+
 			base.Update(gameTime);
 		}
 
@@ -136,6 +166,12 @@ namespace SampleGame.Controller
 
 			// Draw the player
 			player.Draw(spriteBatch);
+
+			// Draw the Enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
 
 			// Stop drawing
 			spriteBatch.End();
@@ -172,6 +208,50 @@ namespace SampleGame.Controller
 			// Make sure that the player does not go out of bounds
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
 			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+		}
+
+		public void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		public void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				}
+			}
 		}
 	}
 }
